be446254e1f948c910d7c8886cc7ec0d69d038fd
Erik9507 <eriknorell1995@gmail.com>
2021-11-16 08:54:45 +0100

    Add project files.

---
+++ Labb 4 – Collection generics/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;                       //Erik Norell Sut21
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace Labb_4___Collection_generics
+{
+    public class Employee
+    {
+        public int _ID = 0;
+        public string _FirstName = "No first name";
+        public string _LastName = "No last name";
+        public string _Gender = "No gender";
+        public int _YearlySalary = 0;
+
+
+        public Employee(int ID, string FirstName, string LastName, string Gender, int YearlySalary)
+        {
+            _ID = ID;
+            _FirstName = FirstName;
+            _LastName = LastName;
+            _Gender = Gender;
+            _YearlySalary = YearlySalary;
+        }
+        
+    }
+}
